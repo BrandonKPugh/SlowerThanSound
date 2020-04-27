bda2303993e9562dcddb7a004745501e8e8011e3
Brandon Pugh <BrandonKPugh@Gmail.com>
2020-04-27 02:12:37 -0500

    Added test progress bar and button for display purposes

--- MonoGameWindowsStarter/Game1.cs
+++ MonoGameWindowsStarter/Game1.cs
@@ -16,6 +16,11 @@ namespace MonoGameWindowsStarter
         public Grid Grid;
         public Ship Ship;
 
+        // Temporary:
+        Button BuildModeButton;
+        ProgressBar HealthBar;
+        int frame = 0;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -42,14 +47,14 @@ namespace MonoGameWindowsStarter
             Ship = new Ship();
             Ship.Initialize(Config.COMPONENTS);
 
-            /*
+            //*
             Rectangle loc = new Rectangle(Config.PRIMARY_BUTTON_X, Config.PRIMARY_BUTTON_Y, Config.PRIMARY_BUTTON_WIDTH, Config.PRIMARY_BUTTON_HEIGHT);
             BuildModeButton = new Button(loc, "Build Mode", Color.ForestGreen);
 
             loc.Y -= 200;
-            HealthBar = new ProgressBar(loc, Color.DarkGray, Color.LightYellow, "Test Text");
+            HealthBar = new ProgressBar(loc, Color.DarkGray, Color.LightYellow, 1.0f, "Test Text");
             HealthBar.Initialize();
-            */
+            //*/
 
             this.IsMouseVisible = true;
             base.Initialize();
@@ -74,7 +79,7 @@ namespace MonoGameWindowsStarter
             Texture2D structureTexture = Content.Load<Texture2D>("Structure");
             textures.Add(Component.Component_Type.Structure, structureTexture);
 
-            /*
+            //*
             Texture2D buttonTexture = Content.Load<Texture2D>("Button");
             SpriteFont font = Content.Load<SpriteFont>("DebugFont");
             BuildModeButton.LoadContent(font, buttonTexture);
@@ -82,7 +87,7 @@ namespace MonoGameWindowsStarter
             Texture2D barTexture = Content.Load<Texture2D>("Progress_Bar");
             Texture2D pixel = Content.Load<Texture2D>("pixel");
             HealthBar.LoadContent(barTexture, pixel, pixel, font);
-            */
+            //*/
 
             Ship.LoadContent(textures);
         }
@@ -129,12 +134,12 @@ namespace MonoGameWindowsStarter
                 }
             }
 
-            /*
+            //*
             HealthBar.Update((1000 - frame) / 1000f);
             frame++;
             if (frame > 1000)
                 frame = 0;
-            */
+            //*/
 
             base.Update(gameTime);
         }
@@ -151,11 +156,11 @@ namespace MonoGameWindowsStarter
 
             Ship.Draw(spriteBatch, Grid.Info);
 
-            /*
+            //*
             BuildModeButton.Draw(spriteBatch);
 
             HealthBar.Draw(spriteBatch);
-            */
+            //*/
 
             base.Draw(gameTime);
         }
