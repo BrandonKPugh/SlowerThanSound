a529ee9fcee301d261c7f331342bd8116d151225
dhriffel <dhriffel@ksu.edu>
2020-04-30 18:34:57 -0500

    Shifted game items into GameState class. New Game button leads into gameplay now

--- MonoGameWindowsStarter/Game1.cs
+++ MonoGameWindowsStarter/Game1.cs
@@ -23,13 +23,6 @@ namespace MonoGameWindowsStarter
             _nextState = state;
         }
 
-        public Grid Grid;
-        public Ship Ship;
-
-        // Temporary:
-        Button BuildModeButton;
-        ProgressBar HealthBar;
-        int frame = 0;
 
         public Game1()
         {
@@ -51,20 +44,7 @@ namespace MonoGameWindowsStarter
             graphics.PreferredBackBufferHeight = Config.GAME_HEIGHT;
             graphics.ApplyChanges();
 
-            Grid = new Grid(Config.GRID_COUNT_X, Config.GRID_COUNT_Y);
-            Grid.Initialize(Config.GRID_DESTINATION);
-
-            Ship = new Ship();
-            Ship.Initialize(Config.COMPONENTS);
-
-            //*
-            Rectangle loc = new Rectangle(Config.PRIMARY_BUTTON_X, Config.PRIMARY_BUTTON_Y, Config.PRIMARY_BUTTON_WIDTH, Config.PRIMARY_BUTTON_HEIGHT);
-            BuildModeButton = new Button(loc, "Build Mode", Color.ForestGreen);
-
-            loc.Y -= 200;
-            HealthBar = new ProgressBar(loc, Color.DarkGray, Color.LightYellow, 1.0f, "Test Text");
-            HealthBar.Initialize();
-            //*/
+            
 
             this.IsMouseVisible = true;
             base.Initialize();
@@ -77,29 +57,6 @@ namespace MonoGameWindowsStarter
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            Texture2D tileTexture = Content.Load<Texture2D>("Tile");
-            Grid.LoadContent(tileTexture);
-
-            Dictionary<Component.Component_Type, Texture2D> textures = new Dictionary<Component.Component_Type, Texture2D>();
-            //Texture2D componentTexture = Content.Load<Texture2D>("Component");
-            //textures.Add(Component.Component_Type.Generic, componentTexture);
-            Texture2D weaponTexture = Content.Load<Texture2D>("Component_Weapon");
-            textures.Add(Component.Component_Type.Weapon, weaponTexture);
-            Texture2D structureTexture = Content.Load<Texture2D>("Structure");
-            textures.Add(Component.Component_Type.Structure, structureTexture);
-
-            //*
-            Texture2D buttonTexture = Content.Load<Texture2D>("Button");
-            SpriteFont font = Content.Load<SpriteFont>("DebugFont");
-            BuildModeButton.LoadContent(font, buttonTexture);
-
-            Texture2D barTexture = Content.Load<Texture2D>("Progress_Bar");
-            Texture2D pixel = Content.Load<Texture2D>("pixel");
-            HealthBar.LoadContent(barTexture, pixel, pixel, font);
-            //*/
-
-            Ship.LoadContent(textures);
 
             _currentState = new MenuState(this, graphics.GraphicsDevice, Content);
         }
@@ -123,35 +80,6 @@ namespace MonoGameWindowsStarter
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            if(Mouse.GetState().LeftButton == ButtonState.Pressed)
-            {
-                int x = Mouse.GetState().X;
-                int y = Mouse.GetState().Y;
-                if(Grid.PixelToTile(x, y, out int tileX, out int tileY))
-                {
-                    Component found = null;
-                    foreach(Component c in Ship.GetComponents())
-                    {
-                        if(c.X == tileX && c.Y == tileY)
-                        {
-                            found = c;
-                            break;
-                        }
-                    }
-                    if(found == null)
-                    {
-                        Component newComponent = new WeaponComponent(tileX, tileY, Config.COMPONENT_WEAPON_COLOR);
-                        Ship.AddComponent(newComponent);
-                    }
-                }
-            }
-
-            //*
-            HealthBar.Update((1000 - frame) / 1000f);
-            frame++;
-            if (frame > 1000)
-                frame = 0;
-            //*/
 
             if (_nextState != null)
             {
@@ -175,17 +103,8 @@ namespace MonoGameWindowsStarter
         {
             GraphicsDevice.Clear(Config.BACKGROUND_COLOR);
 
-            Grid.Draw(spriteBatch);
-
-            Ship.Draw(spriteBatch, Grid.Info);
-
             _currentState.Draw(gameTime, spriteBatch);
 
-            //*
-            BuildModeButton.Draw(spriteBatch);
-
-            HealthBar.Draw(spriteBatch);
-            //*/
 
             base.Draw(gameTime);
         }
--- MonoGameWindowsStarter/States/GameState.cs
+++ MonoGameWindowsStarter/States/GameState.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +12,64 @@ namespace MonoGameWindowsStarter.States
 {
     public class GameState : State
     {
+        public Grid Grid;
+        public Ship Ship;
+
+        // Temporary:
+        Button BuildModeButton;
+        ProgressBar HealthBar;
+        int frame = 0;
+
         public GameState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
           : base(game, graphicsDevice, content)
         {
+            Grid = new Grid(Config.GRID_COUNT_X, Config.GRID_COUNT_Y);
+            Grid.Initialize(Config.GRID_DESTINATION);
+
+            Ship = new Ship();
+            Ship.Initialize(Config.COMPONENTS);
+
+            //*
+            Rectangle loc = new Rectangle(Config.PRIMARY_BUTTON_X, Config.PRIMARY_BUTTON_Y, Config.PRIMARY_BUTTON_WIDTH, Config.PRIMARY_BUTTON_HEIGHT);
+            BuildModeButton = new Button(loc, "Build Mode", Color.ForestGreen);
+
+            loc.Y -= 200;
+            HealthBar = new ProgressBar(loc, Color.DarkGray, Color.LightYellow, 1.0f, "Test Text");
+            HealthBar.Initialize();
+
+            Texture2D tileTexture = content.Load<Texture2D>("Tile");
+            Grid.LoadContent(tileTexture);
+
+            Dictionary<Component.Component_Type, Texture2D> textures = new Dictionary<Component.Component_Type, Texture2D>();
+            //Texture2D componentTexture = Content.Load<Texture2D>("Component");
+            //textures.Add(Component.Component_Type.Generic, componentTexture);
+            Texture2D weaponTexture = content.Load<Texture2D>("Component_Weapon");
+            textures.Add(Component.Component_Type.Weapon, weaponTexture);
+            Texture2D structureTexture = content.Load<Texture2D>("Structure");
+            textures.Add(Component.Component_Type.Structure, structureTexture);
+
+            //*
+            Texture2D buttonTexture = content.Load<Texture2D>("Button");
+            SpriteFont font = content.Load<SpriteFont>("DebugFont");
+            BuildModeButton.LoadContent(font, buttonTexture);
+
+            Texture2D barTexture = content.Load<Texture2D>("Progress_Bar");
+            Texture2D pixel = content.Load<Texture2D>("pixel");
+            HealthBar.LoadContent(barTexture, pixel, pixel, font);
+            //*/
+
+            Ship.LoadContent(textures);
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
+            Grid.Draw(spriteBatch);
 
+            Ship.Draw(spriteBatch, Grid.Info);
+
+            BuildModeButton.Draw(spriteBatch);
+
+            HealthBar.Draw(spriteBatch);
         }
 
         public override void PostUpdate(GameTime gameTime)
@@ -28,7 +79,32 @@ namespace MonoGameWindowsStarter.States
 
         public override void Update(GameTime gameTime)
         {
-
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                int x = Mouse.GetState().X;
+                int y = Mouse.GetState().Y;
+                if (Grid.PixelToTile(x, y, out int tileX, out int tileY))
+                {
+                    Component found = null;
+                    foreach (Component c in Ship.GetComponents())
+                    {
+                        if (c.X == tileX && c.Y == tileY)
+                        {
+                            found = c;
+                            break;
+                        }
+                    }
+                    if (found == null)
+                    {
+                        Component newComponent = new WeaponComponent(tileX, tileY, Config.COMPONENT_WEAPON_COLOR);
+                        Ship.AddComponent(newComponent);
+                    }
+                }
+            }
+            HealthBar.Update((1000 - frame) / 1000f);
+            frame++;
+            if (frame > 1000)
+                frame = 0;
         }
     }
 }
